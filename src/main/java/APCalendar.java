27eedf34f7e0fb359c95a3ecd136f4cb4e889aec
isabelqiu11 <qiu.isabel40@gmail.com>
2025-05-01 11:50:21 -0700

    Update APCalendar.java

--- src/main/java/APCalendar.java
+++ src/main/java/APCalendar.java
@@ -57,7 +57,7 @@ public class APCalendar
   public static int dayOfWeek(int month, int day, int year)
   {
     int firstday = firstDayofYear(year);
-    int dayYear = dayOfYear(month,day,year):
+    int dayYear = dayOfYear(month,day,year);
       return ((firstday + dayYear-1)%7);
   }
 }
